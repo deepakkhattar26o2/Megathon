46e56786c52e2d9133f0cb44973377184063d172
deepakkhattar26o2 <74506752+deepakkhattar26o2@users.noreply.github.com>
2022-01-17 06:43:49 +0530

    Add files via upload

---
+++ src/middleware/check-auth.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken')
+module.exports = (req, res, next)=>{
+    try{
+        const token = req.headers.authorization.split(" ")[1]
+        console.log(token.email)
+        const decoded = jwt.verify(token, 'secret_key')
+        req.userData = decoded
+        next();
+    }
+    catch(err){
+        return res.status(401).json({Message: 'Auth Failed!'})
+    }
+}
