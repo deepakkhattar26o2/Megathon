b69538256039d80090ab69198e76630c97326ea7
deepakkhattar26o2 <74506752+deepakkhattar26o2@users.noreply.github.com>
2022-01-20 15:52:53 +0530

    Update User.js

--- src/Routes/User.js
+++ src/Routes/User.js
@@ -1,16 +1,16 @@
 const Express = require('express')
 const router = Express.Router();
 const userController = require('../Controllers/User')
-const User = require('../Models/User')
-const bcr = require('bcrypt')
-const jwt = require("jsonwebtoken")
 
-router.post('/signup', userController.signup)
 
-router.delete('/delete', userController.deleteAccount)
+router.get('/', userController.getAll)
 
 router.get('/:userName', userController.getSingleUser)
 
+router.post('/signup', userController.signup)
+
 router.post('/login', userController.userLogin)
+
+router.delete('/delete', userController.deleteAccount)
 
 module.exports = router;
