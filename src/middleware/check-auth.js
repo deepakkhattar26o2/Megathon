6bea3a231328431d5b8c51e338ffe32efec44798
deepakkhattar26o2 <74506752+deepakkhattar26o2@users.noreply.github.com>
2022-01-20 15:51:31 +0530

    Update check-auth.js

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 module.exports = (req, res, next)=>{
     try{
-        const token = req.headers.authorization.split(" ")[1]
+        const token = req.headers.authorization
         console.log(token.email)
         const decoded = jwt.verify(token, process.env.myKey)
         req.userData = decoded
