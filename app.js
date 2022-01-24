9f7ec3fc598bfe63a34c3673abd26bfb887e9a70
deepakkhattar26o2 <74506752+deepakkhattar26o2@users.noreply.github.com>
2022-01-24 17:03:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const app = express();
 const dbConnect = require('./db')
 const bodyParser = require('body-parser')
 const userRoute = require('./src/Routes/User')
+const hackathonRouter = require('./src/Routes/Hackathon')
+const blogRouter = require('./src/Routes/Blog')
 dbConnect();
 app.use(bodyParser.json())
+
 app.get('/', (req, res, next)=>{
     res.status(200).json({
         Message: 'Nothing To See Here Buoy!'
     })
 })
+
 app.use('/user', userRoute)
+
+app.use('/hackathon', hackathonRouter)
+
+app.use('/blog', blogRouter)
 
 module.exports = app;
