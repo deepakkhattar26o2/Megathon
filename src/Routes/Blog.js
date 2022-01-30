b694e242020c01501a818ca306bd75943dc185f1
deepakkhattar26o2 <74506752+deepakkhattar26o2@users.noreply.github.com>
2022-01-30 17:05:29 +0530

    Update Blog.js

--- src/Routes/Blog.js
+++ src/Routes/Blog.js
@@ -4,9 +4,9 @@ const blogController = require('../Controllers/Blog')
 
 router.post("/create", auth,blogController.createBlog );
 
-router.get('/', auth, blogController.getAll)
+router.get('/',  blogController.getAll)
 
-router.get('/:title', auth, blogController.getByTitle)
+router.get('/:title', blogController.getByTitle)
 
 router.delete('/:title', auth, blogController.deleteByTitle)
 
